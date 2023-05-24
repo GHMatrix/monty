c01c6d4cce2e083b8d9668e3a05c76fa45f556fd
GHMatrix <gsmensah@yahoo.com>
2023-05-24 11:00:27 -0700

    Do nothing

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * st_nop -program performs no action
+ * @head: head of stack
+ * @counter: line_number
+ * Return: void
+ */
+void st_nop(stack_t **head, unsigned int counter)
+{
+	(void)counter;
+	(void)head;
+}
