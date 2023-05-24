8ced0838b5f8f60f910d895c0046d876a69d7bb4
GHMatrix <gsmensah@yahoo.com>
2023-05-24 10:51:55 -0700

    Printing values at top of stack

---
+++ pint.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * st_pint - program prints top
+ * @head: head of stack
+ * @counter: number of line
+ * Return: void
+ */
+void st_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+	fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+	fclose(bus.file);
+	free(bus.content);
+	free_stack(*head);
+	exit(EXIT_FAILURE);
+	}
+
+	stack_t *top = *head;
+
+	printf("%d\n", top->n);
+}
